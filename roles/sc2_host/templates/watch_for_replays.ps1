62e5fb49fd2e4f686f1d57de3c937d084bd6e49b
Cullen Taylor <cullentaylor@outlook.com>
2017-03-07 14:23:34 -0600

    Add tasks to finish sc2_host role
    * Ensures execution policy is set to unrestricted
    * Ensures needed directories exist
    * Templates powershell script and kicks it off to watch for new replay
      files.

---
+++ roles/sc2_host/templates/watch_for_replays.ps1
@@ -0,0 +1,12 @@
+$sb={
+     $name = $Event.SourceEventArgs.FullPath
+     $changeType = $Event.SourceEventArgs.ChangeType
+     $timeStamp = $Event.TimeGenerated
+     # TODO make this a REST API call pushing to object storage container
+     Write-Host "The file $name was $changeType at $timeStamp" -fore green
+     $event
+}
+
+$fsw=New-Object IO.FileSystemWatcher('{{ replays_dir }}', '*.SC2Replay*')
+$fsw.IncludeSubdirectories=$true
+Register-ObjectEvent -InputObject $fsw -EventName Created -SourceIdentifier FileCreated -Action $sb
